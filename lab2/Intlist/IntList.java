406b8012f775f1e0b49ca97538811164f4569cb4
jzhao <jianyu.zhao@niji.fr>
2021-02-07 16:21:28 +0100

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,18 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if(A==null){
+            return B;
+        }
+        if(B==null){
+            return A;
+        }
+        IntList ptr = A;
+        while(ptr.rest!=null){
+            ptr=ptr.rest;
+        }
+        ptr.rest=B;
+        return A;
     }
 
     /**
@@ -90,8 +100,26 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if(A==null){
+            return B;
+        }
+        if(B==null){
+            return A;
+        }
+        IntList C = new IntList(A.first, null);
+        IntList ptr = C;
+        A=A.rest;
+        while(A!=null){
+            ptr.rest = new IntList(A.first, null);
+            A=A.rest;
+            ptr=ptr.rest;
+        }
+        while(B!=null){
+            ptr.rest = new IntList(B.first, null);
+            B=B.rest;
+            ptr=ptr.rest;
+        }
+        return C;
     }
 
 
